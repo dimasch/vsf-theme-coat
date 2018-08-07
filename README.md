b66fbeca12abd80798e0a633a18ae492a327d487
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2018-08-07 16:11:33 +0400

    Bump version to 0.1.0, fixed readme.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# The Coat Theme for [Vue Storefront](https://github.com/DivanteLtd/vue-storefront). 
+# The Coat Theme for [Vue Storefront](https://github.com/DivanteLtd/vue-storefront).
 Extending atomic concept with Utility-First Tailwindcss and styleguide out of the box
+
+# Styleguide
+Additional page accessed by url `/styleguide` with all UI components on the one page for rapid theme styling!
 
 ## Configuration
 Need pass `require('tailwindcss')(themeRoot + '/tailwind.js'),` to `./core/build/webpack.base.config.js`
--- router/index.js
+++ router/index.js
@@ -38,7 +38,8 @@ let routes = [
   { name: 'compare', path: '/compare', component: Compare, props: {title: 'Compare Products'} },
   { name: 'page-not-found', path: '/page-not-found', component: PageNotFound },
   { name: 'custom-cms-page', path: '/custom-cms-page', component: CustomCmsPage },
-  { name: 'cms-page-sync', path: '/cms-page-sync', component: CmsData, props: {identifier: 'about-us', type: 'Page', sync: true} }
+  { name: 'cms-page-sync', path: '/cms-page-sync', component: CmsData, props: {identifier: 'about-us', type: 'Page', sync: true} },
+  { name: 'styleguide', path: '/styleguide', component: StyleGuide }
 ]
 if (!config.products.useShortCatalogUrls) {
   routes = routes.concat([{ name: 'virtual-product', path: '/p/:parentSku/:slug', component: Product }, // :sku param can be marked as optional with ":sku?" (https://github.com/vuejs/vue-router/blob/dev/examples/route-matching/app.js#L16), but it requires a lot of work to adjust the rest of the site
